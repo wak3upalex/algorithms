295ec7e5f7a36c5b1ac27884ac9ac04b33137488
Aleksei Dmitriev <aleshadmi@mail.ru>
2024-11-07 15:24:14 +0300

    Create a tree

--- dataStructures/tree/tree.py
+++ dataStructures/tree/tree.py
@@ -5,3 +5,18 @@ class Node:
 
     def add_child(self, child_node):
         self.children.append(child_node)
+
+
+# tree creation
+root = Node("A")
+node_b = Node("B")
+node_c = Node("C")
+node_d = Node("D")
+node_e = Node("E")
+node_f = Node("F")
+
+root.add_child(node_b)
+root.add_child(node_c)
+node_b.add_child(node_d)
+node_b.add_child(node_e)
+node_c.add_child(node_f)
