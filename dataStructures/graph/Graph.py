8cb441885bba97a6ba65bba6e49491c805dcb536
Aleksei Dmitriev <aleshadmi@mail.ru>
2024-11-08 11:03:39 +0300

    Create Graph

--- dataStructures/graph/Graph.py
+++ dataStructures/graph/Graph.py
@@ -9,3 +9,13 @@ class Graph:
     def add_edge(self, from_node, to_node):
         if from_node in self.nodes and to_node in self.nodes:
             self.nodes[from_node].append(to_node)
+
+# creating graph
+graph = Graph()
+for node in ["A", "B", "C", "D"]:
+    graph.add_node(node)
+
+graph.add_edge("A", "B")
+graph.add_edge("A", "C")
+graph.add_edge("B", "D")
+graph.add_edge("C", "D")
